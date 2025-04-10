d27312be8cdf8b451ca9934981148b003705fc80
Дмитро Дем'яник <67798322+DemaReaktor@users.noreply.github.com>
2025-04-10 06:21:31 +0300

    fix: bugs

--- task1/main.py
+++ task1/main.py
@@ -1,17 +1,14 @@
-import math
 from utilites import *
 import logging
-import time
 import dronekit
 
 
 logger = logging.getLogger(__name__)
 
 
-height = 100
+height = 5
 target_geo_location = 50.443326, 30.4480785
-target_location_error = 5
-speed = 10
+target_location_error = 2
 connection_string = "tcp:127.0.0.1:5762"
 finish_yaw = 350
 
@@ -28,43 +25,22 @@ logging.basicConfig(level=logging.INFO,
 drone = dronekit.connect(connection_string, wait_ready=True)
 prepare(drone)
 while drone.location.global_relative_frame.alt < height:
-    drone.channels.overrides['3'] = 1800
+    drone.channels.overrides['3'] = 2000
 logger.info('vehicle took off')
-while True:
-    drone.channels.overrides['3'] = 1500
-    target_location = dronekit.LocationGlobalRelative(target_geo_location[0], target_geo_location[1], height)
-    rotation = get_rotation(drone, target_location)
-    if math.fabs(rotation - degrees(drone.attitude.yaw)) < 5:
-        break
-    rotate(drone, True)
-    time.sleep(0.2)
+
+target_location = dronekit.LocationGlobalRelative(target_geo_location[0], target_geo_location[1], height)
+rotate_to_yaw(drone, get_rotation(drone, target_location), 5)
 logger.info('vehicle is rotated')
 
-drone.channels.overrides['4'] = None
-target_location = dronekit.LocationGlobalRelative(target_geo_location[0], target_geo_location[1], height)
+move_to(drone, target_location, target_location_error, 3)
 
-while True:
-    drone.channels.overrides['3'] = 1500
-    north, east, down = get_ned_vector(target_location, drone.location.global_frame)
-    length = math.sqrt(north**2 + east**2)
-    if length < target_location_error:
-        break
-    drone.channels.overrides['2'] = max(1000, min(2000, 1500 - int(speed * 100)))
-    rotation = get_rotation(drone, target_location)
-    if math.fabs(rotation - degrees(drone.attitude.yaw)) > 5:
-        rotate(drone, True)
-    else:
-        drone.channels.overrides['4'] = None
-    time.sleep(0.2)
+drone.channels.overrides['2'] = 1500
 logger.info('vehicle has been arrived')
-drone.channels.overrides['2'] = None
 
-while True:
-    if math.fabs((finish_yaw - degrees(drone.attitude.yaw) + 180) % 360 - 180) < 5:
-        break
-    drone.channels.overrides['4'] = 1525 if 0 < (finish_yaw - degrees(drone.attitude.yaw) + 360) % 360 < 180 else 1475
+rotate_to_yaw(drone, finish_yaw, 1)
 
 drone.channels.overrides['4'] = None
 logger.info('finish')
+
 drone.close()
 
--- task1/utilites.py
+++ task1/utilites.py
@@ -1,22 +1,30 @@
+import time
 from math import radians, cos, sin, sqrt, atan2, degrees
 from dronekit import VehicleMode, Vehicle, LocationGlobalRelative
 import logging
+from typing import Tuple
 
 logger = logging.getLogger(__name__)
 
 
-def get_distance_meters(first_geo_location, second_geo_location):
+def keep_height(vehicle: Vehicle):
+    vehicle.channels.overrides['3'] = 1500
+
+
+def get_distance_meters(first_geo_location: LocationGlobalRelative, second_geo_location: LocationGlobalRelative) -> (
+        float):
     radius = 6378137.0
-    lat_delta = radians(second_geo_location[0] - first_geo_location[0])
-    lon_delta = radians(second_geo_location[1] - first_geo_location[1])
-    a = (sin(lat_delta / 2)**2 + cos(radians(first_geo_location[0])) * cos(radians(second_geo_location[1]))
+    lat_delta = radians(second_geo_location.lat - first_geo_location.lat)
+    lon_delta = radians(second_geo_location.lon - first_geo_location.lon)
+    a = (sin(lat_delta / 2)**2 + cos(radians(first_geo_location.lat)) * cos(radians(second_geo_location.lon))
          * sin(lon_delta / 2)**2)
     return 2 * atan2(sqrt(a), sqrt(1 - a)) * radius
 
 
-def get_ned_vector(target_location, home_location):
-    dNorth = get_distance_meters((home_location.lat, home_location.lon), (target_location.lat, home_location.lon))
-    dEast = get_distance_meters((home_location.lat, home_location.lon), (home_location.lat, target_location.lon))
+def get_vector(home_location: LocationGlobalRelative, target_location: LocationGlobalRelative) -> (
+        Tuple[float, float, float]):
+    dNorth = get_distance_meters(home_location, LocationGlobalRelative(target_location.lat, home_location.lon))
+    dEast = get_distance_meters(home_location, LocationGlobalRelative(home_location.lat, target_location.lon))
     if target_location.lat < home_location.lat:
         dNorth *= -1
     if target_location.lon < home_location.lon:
@@ -39,10 +47,34 @@ def prepare(vehicle: Vehicle):
     logger.info('vehicle is armed')
 
 
-def rotate(vehicle: Vehicle, right: bool = True):
-    vehicle.channels.overrides['4'] = 1540 if right else 1460
+def move_to(vehicle: Vehicle, target_location: LocationGlobalRelative, target_location_error: float = 5,
+    rotation_error: float = 5):
+    target_distance = get_distance_meters(vehicle.location.global_frame, target_location)
+    rotation = get_rotation_from_yaw(vehicle, get_rotation(vehicle, target_location))
+    while target_distance > target_location_error:
+        keep_height(vehicle)
+        vehicle.channels.overrides['4'] = (1500 + int(rotation * 0.6 + (20 if rotation >= 0 else -20))) if (
+            not -rotation_error < rotation < rotation_error) else None
+        vehicle.channels.overrides['2'] = max(1000, 1470 - int(target_distance * 4))
+        time.sleep(0.1)
+        rotation = get_rotation_from_yaw(vehicle, get_rotation(vehicle, target_location))
+        target_distance = get_distance_meters(vehicle.location.global_frame, target_location)
+
+
+def rotate_to_yaw(vehicle: Vehicle, yaw: float, rotation_error: float = 1):
+    rotation = get_rotation_from_yaw(vehicle, yaw)
+    while not -rotation_error < rotation < rotation_error:
+        keep_height(vehicle)
+        vehicle.channels.overrides['4'] = (1500 + int(rotation * 0.4 + (25 if rotation >= 0 else -25))) if (
+            not -rotation_error < rotation < rotation_error) else 1500
+        time.sleep(0.1)
+        rotation = get_rotation_from_yaw(vehicle, yaw)
+
+
+def get_rotation_from_yaw(vehicle: Vehicle, yaw: float):
+    return (yaw - degrees(vehicle.attitude.yaw) + 540) % 360 - 180
 
 
 def get_rotation(vehicle: Vehicle, position: LocationGlobalRelative) -> float:
-    vector = get_ned_vector(position, vehicle.location.global_frame)
-    return degrees(atan2(vector[1], vector[0]))
+    vector = get_vector(vehicle.location.global_frame, position)
+    return (450 - degrees(atan2(vector[0], vector[1]))) % 360
